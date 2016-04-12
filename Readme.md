6cd9e20eb15e11c9030755f1bb734f75366f8bdc
memsharded <james@conan.io>
2016-04-12 23:46:38 +0200

    removed 32bits linux, MSVC11=>10, added readme

---
+++ Readme.md
@@ -0,0 +1,14 @@
+Conan package for Google Protocol Buffers
+--------------------------------------------
+
+[![Build Status](https://travis-ci.org/memsharded/conan-protobuf.svg?branch=master)](https://travis-ci.org/memsharded/conan-protobuf)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/3mj06qm32q2c0pgl?svg=true)](https://ci.appveyor.com/project/memsharded/conan-protobuf)
+
+
+Still in development...
+
+Package in: https://www.conan.io/source/Protobuf/2.6.1/memsharded/testing
+
+
+
--- build.py
+++ build.py
@@ -3,6 +3,11 @@ import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="memsharded", channel="testing",
-                                 visual_versions=["10", "11", "12", "14"])
+                                 visual_versions=["10", "12", "14"])
     builder.add_common_builds(shared_option_name="Protobuf:shared")
+    filtered_builds = []
+    for settings, options in builder.builds:
+        if settings["arch"] == "x86_64":
+             filtered_builds.append([settings, options])
+    builder.builds = filtered_builds
     builder.run()
