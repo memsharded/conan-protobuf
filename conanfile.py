277f1512201314571bba53c3f2f584ee24662920
Alesiani Marco <marcodiiga@users.noreply.github.com>
2016-12-19 14:49:39 +0100

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -59,7 +59,7 @@ class ProtobufConan(ConanFile):
             if not self.options.shared:
                 args += ['--disable-shared']
             
-            args += ('"CFLAGS=-fPIC" "CXXFLAGS=-fPIC"' if self.options.shared else '')
+            args += ('"CFLAGS=-fPIC" "CXXFLAGS=-fPIC"' if (self.options.shared || self.options.fPIC) else '')
 
             self.run("cd protobuf-2.6.1 && %s ./configure %s" % (env.command_line, ' '.join(args)))
             self.run("cd protobuf-2.6.1 && make -j %s" % concurrency)
