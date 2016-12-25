8cfd58b25f4f3a07470c2d8a5dedd8b6dcdebd98
Eric Pederson <ericacm+github@gmail.com>
2016-12-25 01:03:46 -0500

    Update to Protobuf 3.0.2

--- change_dylib_names.sh
+++ change_dylib_names.sh
@@ -1,5 +1,13 @@
+#!/usr/bin/env bash
 for BINARY in $(find . -type f | grep '\.dylib$') protoc; do
-     for LINK_DESTINATION in $(otool -L $BINARY | grep libproto | cut -f 1 -d' '); do
-         install_name_tool -change "$LINK_DESTINATION" "@executable_path/$(basename $LINK_DESTINATION)" "$BINARY"
+     for LINK_DESTINATION in $(otool -L ${BINARY} | grep libproto | grep -v : | cut -f 1 -d' '); do
+         BINARY_BASE=$(basename ${BINARY})
+         LINK_BASE=$(basename ${LINK_DESTINATION})
+
+         if [[ ${BINARY_BASE} == ${LINK_BASE} ]]; then
+            install_name_tool -id "@executable_path/${LINK_BASE}" "$BINARY"
+         else
+            install_name_tool -change "${LINK_DESTINATION}" "@executable_path/${LINK_BASE}" "${BINARY}"
+         fi
      done
 done
