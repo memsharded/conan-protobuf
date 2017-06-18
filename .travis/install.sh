fadf10aca5f658e0692215d2feb10238acce7c97
memsharded <james@conan.io>
2017-06-18 23:01:59 +0200

    upgrading CI

--- .travis/install.sh
+++ .travis/install.sh
@@ -21,5 +21,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
+pip install conan
 pip install conan_package_tools # It install conan too
+
 conan user
--- build.py
+++ build.py
@@ -1,14 +1,6 @@
 from conan.packager import ConanMultiPackager
-import platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="memsharded", channel="testing",
-                                 visual_versions=["10", "12", "14"])
+    builder = ConanMultiPackager(username="memsharded", channel="testing", default_archs = ["x86_64"])
     builder.add_common_builds(shared_option_name="Protobuf:shared")
-    filtered_builds = []
-    for settings, options, env_vars, build_requires in builder.builds:
-        if settings["arch"] == "x86_64":
-             filtered_builds.append([settings, options, env_vars, build_requires])
-
-    builder.builds = filtered_builds
     builder.run()
