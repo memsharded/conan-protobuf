f52cb43c7eb990b7587388fe240529d4978327b5
memsharded <james@conan.io>
2017-06-18 23:23:36 +0200

    fixed archs

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="memsharded", channel="testing", default_archs = ["x86_64"])
+    builder = ConanMultiPackager(username="memsharded", channel="testing", archs = ["x86_64"])
     builder.add_common_builds(shared_option_name="Protobuf:shared")
     builder.run()
